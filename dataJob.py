cf1e287d1a55b1cd65a21a037aa37001320e7ed8
huy <huy@localhost.localdomain>
2017-12-05 08:13:43 -0500

    -finished database

--- dataJob.py
+++ dataJob.py
@@ -73,12 +73,12 @@ def create_tbWorkflow():
 
 #insert job table
 def insert_job(host=None):
+    start = time.time()
     global strQuery
     strQuery += "truncate job; insert into job (jid, host, state, status, prog, ver, startdate, enddate) values"
     response_xml = Job().get_job_xml()
     sql = Job().parse_xml_2_query(response_xml, host)[:-1]
     sql = strQuery + sql + ";\n commit"
-    print sql
     command = command_sql(sql)
     try:
         os.system(command)
@@ -88,10 +88,11 @@ def insert_job(host=None):
     finally:
         strQuery =''
     File('sql/').write_log("job.sql", sql)
+    print ('End Job: ', time.time() - start)
 
 #insert param table
 def insert_param_thread(lstJid=None):
-
+    start = time.time()
     lstJob = lstJid
     global strQuery
     strQuery += "truncate job_param; insert into job_param(jid, host, name, wid) values "
@@ -103,7 +104,6 @@ def insert_param_thread(lstJid=None):
         job.start()
     mainQ.join()
     sql = strQuery[:-1] + ";\ncommit"
-    print sql
     command = command_sql(sql)
     try:
         os.system(command)
@@ -113,6 +113,7 @@ def insert_param_thread(lstJid=None):
     finally:
         strQuery = ''
     File("sql/").write_log("param_job.sql", sql)
+    print ('End job_param: ', time.time() - start)
 
 def insert_param(lstJid):
     sql = """truncate job_param;"""
@@ -120,6 +121,7 @@ def insert_param(lstJid):
     for job in lstJob:
         param = JobDetail(job['jid'], job['host'])
         sql += param.parse_xml_2_query(param.get_param())
+    print sql
     command = command_sql(sql)
     try:
         os.system(command)
@@ -141,10 +143,10 @@ def get_lstJob_id():
 #insert workflow table
 def insert_workflow(host=None):
     start = time.time()
-
     sql = "truncate workflow; insert into  workflow(wid, name, host, pubver, priver) values"
     response_xml = Workflow(host)
-    sql += response_xml.parse_xml_2_query(response_xml.get_workflow())[:1]
+    sql += response_xml.parse_xml_2_query(response_xml.get_workflow())[:-1]+";\ncommit"
+    # print sql
     command = command_sql(sql)
     try:
         os.system(command)
@@ -152,14 +154,10 @@ def insert_workflow(host=None):
     except Exception as e:
         print e
     File("sql/").write_log("workflow.sql", sql)
-    print ('End job: ', time.time() - start)
+    print ('End workflow: ', time.time() - start)
 
 def command_sql(sql):
     return """mysql -u%s -p'%s' %s -h %s -e "%s" """%(osDb.DATABASE_USER, osDb.DATABASE_PASSWORD, osDb.DATABASE_NAME, osDb.DATABASE_HOST, sql)
-
-def write_sql_file(filename):
-
-    return True
 
 #create_tbJob()
 #create_tbParam()
@@ -169,17 +167,23 @@ def write_sql_file(filename):
 # insert_workflow(osDb.THOMSON_HOST)
 #insert_param(get_lstJob_id())
 def main():
-    threads = []
-    thread_job = threading.Thread(target='insert_job', kwargs={'host': osDb.THOMSON_HOST})
-    thread_job.start()
-    thread_param = threading.Thread(target='insert_param_thread', kwargs={'lstJid':get_lstJob_id()})
-    thread_param.start()
-    thread_workflow = threading.Thread(target='insert_workflow', kwargs={'host': osDb.THOMSON_HOST})
-    thread_workflow.start()
-    threads.append(thread_workflow)
-    threads.append(thread_job)
-    threads.append( thread_param)
-    for t in threads:
-        t.join()
+    # threads = []
+    # thread_job = threading.Thread(target='insert_job', kwargs={'host':osDb.THOMSON_HOST})
+    # thread_job.start()
+    # # thread_param = threading.Thread(target='insert_param_thread', kwargs={get_lstJob_id()})
+    # # thread_param.start()
+    # # thread_workflow = threading.Thread(target='insert_workflow', kwargs={osDb.THOMSON_HOST})
+    # # thread_workflow.start()
+    # # threads.append(thread_workflow)
+    # threads.append(thread_job)
+    # # threads.append( thread_param)
+    # for t in threads:
+    #     t.join()
+    insert_job(osDb.THOMSON_HOST)
+    time.sleep(0.02)
+    insert_param_thread(get_lstJob_id())
+    insert_workflow(osDb.THOMSON_HOST)
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(1.5)
